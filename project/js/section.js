b3164d27fb3023895aed52391492d14e1d044566
hafsa ahmed <sooahmed83@yahoo.com>
2021-11-24 19:54:25 +0200

    Gulp Web Pack

---
+++ project/js/section.js
@@ -0,0 +1,9 @@
+let section= document.getElementsByClassName("hero01")[0]
+
+
+ const element = document.createElement("h1");
+
+element.innerHTML = "Hello From Hafsaaaa";
+section.appendChild(element)
+
+// this is a comment
